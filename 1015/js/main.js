87af0d7591eb5426cfbfe467de68bb94dd8c99e2
sv7082408 <sv7082408@gmail.com>
2021-10-18 00:55:46 +0300

    sent1018

---
+++ 1015/js/main.js
@@ -0,0 +1,62 @@
+$(function(){
+				var field = $('#green')[0].getBoundingClientRect();
+                var step = 10;
+                var borders = {
+                    left: 0,
+                    top: 0,
+                    // вычисляем правый и нижний края
+                    right: field.width,
+                    bottom: field.height,
+                }
+               
+                $('#red').on('mouseover', function(e){
+                    let coords = e.target.getBoundingClientRect();
+                    // вычисляем центр красного блока
+                    let center = {
+                        x: coords.left + coords.width / 2,
+                        y: coords.top + coords.height / 2,
+                    }
+                    // сравниваем координаты центра и координаты курсора мыши
+                    // определяем в каком направлении двигаться
+                    let delta = {
+                        x: Math.sign(center.x - e.clientX),
+                        y: Math.sign(center.y - e.clientY),
+                    }
+                    // определяем положение верхнего левого угла блока относительно его родителя
+                    let oldplace = {
+                        left: $('#red')[0].offsetLeft,
+                        top: $('#red')[0].offsetTop,
+                    }
+                    // определяем положение верхнего левого угла блока относительно его родителя и вычисляем новое положение
+                    let newplace = {
+                        left: oldplace.left + step * delta.clientX,
+                        top: oldplace.top + step * delta.clientY,
+                    }
+                    // проверяем нет ли касания границ поля.
+                    if (borders.left > newplace.left) {
+                        newplace.left = borders.left;
+                    } else if (borders.right - coords.width < newplace.left) {
+                        newplace.left = borders.right - coords.width - 2;
+                    }
+                    if (borders.top > newplace.top) {
+                        newplace.top = borders.top;
+                    } else if (borders.bottom - coords.height < newplace.top) {
+                        newplace.top = borders.bottom - coords.height - 2;
+                    }
+                    // прописываем в стили красного блока новое положение
+                    $('#red').css('left', (oldplace.left + 'px'));
+					$('#red').css('top', $('#red').offsetTop + 'px');
+                    let xstep = (newplace.left - oldplace.left) / 1000;
+                    let ystep = (newplace.top - oldplace.top) / 1000;
+                    for (let i = 1; i < 1001; i++) {
+                        $('#red').css('left', (oldplace.left + xstep * i) + 'px');
+                        $('#red').css('top', (oldplace.top + ystep * i) + 'px');
+                        if ((i > 10) && (i < 990)) {
+                            i += 9;
+                            if ((i > 100) && (i < 900)) {
+                                i += 90;
+                            }
+                        }
+                    }
+                });
+});
---
+++ chess/chess.js
@@ -0,0 +1,84 @@
+﻿$(function(){
+    $('td').click(whoIs);
+	$('td').click(markStep);
+});
+
+function whoIs(e) {
+    let hlpstr = '';
+    hlpstr += 'This is ' + e.target.id + '. ';
+    let hlpcls = e.target.classList;
+    if (!hlpcls.contains('white') && !hlpcls.contains('black')) {
+        hlpstr += 'Empty here.';
+    } else {
+        if (hlpcls.contains('white')) {
+            hlpstr += 'White '
+        } else {
+            hlpstr += 'Black '
+        }
+        if (hlpcls.contains('king')) {
+            hlpstr += 'king here.'
+        } else if (hlpcls.contains('queen')) {
+            hlpstr += 'queen here.'
+        } else if (hlpcls.contains('rook')) {
+            hlpstr += 'rook here.'
+        } else if (hlpcls.contains('bishop')) {
+            hlpstr += 'bishop here.'
+        } else if (hlpcls.contains('knight')) {
+            hlpstr += 'knight here.'
+        } else {
+            hlpstr += 'pawn here.';
+        }
+    }
+    console.log(hlpstr);
+}
+
+function markStep(e) {
+    // 1 По клику на клетке пометить ее классом stepfrom.
+	
+    // 2 Если на кликнутой клетке уже есть такой класс, просто убрать его.
+	
+    // 3 Если есть другая клетка с этим классом, пометить кликнутую клетку классом stepto.
+    // 4 Если на кликнутой клетке есть класс stepto, просто убрать его.
+    // 5 Если на доске уже есть другая клетка с классом stepto, убрать классы stepfrom и stepto с ранее помеченных клеток, кликнутую пометить классом stepfrom.
+    // Если после всех манипуляций на доске остались клетки с классами stepfrom и steptoб вывести в консоль объект вида {stepfrom: <cell id>, stepto: <cell id>}
+
+	let isStepFrom = false;
+	let isStepTo = false;
+	document.querySelectorAll('.stepfrom').forEach(function(item){
+		isStepFrom = true;
+	});
+	document.querySelectorAll('.stepto').forEach(function(item){
+		isStepTo = true;
+	});
+	
+	if (isStepFrom == true && isStepTo == false && event.target.className != 'stepfrom') {
+		event.target.className = 'stepto';
+	}
+	else if (isStepFrom == false && event.target.className != 'stepto') {
+		event.target.className = 'stepfrom';
+	}
+	else if (event.target.className == 'stepfrom') {
+		event.target.className = '';
+	}
+	else if (event.target.className == 'stepto') {
+		event.target.className = '';
+	}
+	else if (isStepFrom == true && isStepTo == true && event.target.className != 'stepfrom'	&& event.target.className != 'stepto') {
+		document.querySelectorAll('.stepfrom').forEach(function(item){
+			item.className = '';
+		});
+		document.querySelectorAll('.stepto').forEach(function(item){
+			item.className = '';
+		});	
+		event.target.className = 'stepfrom';
+	}
+	else {
+		document.querySelectorAll('.stepfrom').forEach(function(item){
+			console.log('stepfrom: ' + item.id);
+		});
+		document.querySelectorAll('.stepto').forEach(function(item){
+			console.log('stepto: ' + item.id);
+		});
+	}            
+}
+
